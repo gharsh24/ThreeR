84f51896714f2bff29fe61809ecac46c6f0c5109
Harsh Goyal <harsh.sg360@gmail.com>
2023-12-23 22:49:42 +0530

    organise styles

--- src/Components/movingBg.jsx
+++ src/Components/movingBg.jsx
@@ -4,86 +4,16 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import CountUp from "react-countup";
 import Bgvideo from "./Video/video.mp4"; //video is from Ingrid North
-import "./styles/styles.css";
+import "./styles/style.css";
 
 function MovingBg() {
-  const containerStyle = {
-    position: "relative",
-    overflow: "hidden",
-    marginLeft: "auto",
-    marginRight: "auto",
-    paddingLeft: 0,
-    paddingRight: 0,
-    height: "80vh",
-  };
-
-  const videoStyle = {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    width: "100vw",
-    height: "100%",
-    objectFit: "cover",
-    zIndex: -1,
-  };
-
-  const rowStyle = {
-    color: "white",
-    marginTop: "4rem",
-    marginLeft: "3.5rem",
-    marginRight: "3.5rem",
-  };
-
-  const colStyle = {
-    fontSize: "2rem", // Default font size for small screens
-
-    "@media (minWidth: 576px)": {
-      fontSize: "2.5rem",
-    },
-
-    "@media (minWidth: 768px)": {
-      fontSize: "3rem",
-    },
-    "@media (minWidth: 992px)": {
-      fontSize: "4rem",
-    },
-    "@media (minWidth: 1200px)": {
-      fontSize: "5rem",
-    },
-
-    fontWeight: "bold",
-    marginBottom: "0.8rem",
-  };
-
-  const colStyle2 = {
-    fontSize: "3rem", // Default font size for small screens
-
-    "@media (minWidth: 576px)": {
-      fontSize: "3.5rem",
-    },
-
-    "@media (minWidth: 768px)": {
-      fontSize: "4rem",
-    },
-    "@media (minWidth: 992px)": {
-      fontSize: "5rem",
-    },
-    "@media (minWidth: 1200px)": {
-      fontSize: "6rem",
-    },
-
-    fontWeight: "bold",
-    marginBottom: "0.8rem",
-    marginTop: "0rem",
-  };
-
   return (
-    <Container fluid className="mt- p-0" style={containerStyle}>
-      <video autoPlay loop muted className="bg-vid" style={videoStyle}>
+    <Container fluid className="mt-0 p-0 containerStyle" >
+      <video autoPlay loop muted className="bg-vid videoStyle" >
         <source src={Bgvideo} type="video/mp4" />
       </video>
 
-      <Row style={rowStyle}>
+      <Row className="rowStyle">
         <Col className="d-flex justify-content-center align-items-center">
           {}
         </Col>
@@ -94,61 +24,63 @@ function MovingBg() {
           {/* Adjust values as needed */}
         </Col>
       </Row>
-      <Row style={rowStyle}>
+      <Row className="rowStyle">
         <Col className="d-flex justify-content-center align-items-center"></Col>
 
         <Col className="d-flex justify-content-center align-items-center">
           <Row className="mt-2">
-            <p className="text-center">Number of Trees Planted:</p>
+            <h3 className="text-center">Number of Trees Planted:</h3>
             <CountUp
               end={500}
-              className="text-center"
-              style={colStyle2}
-              duration={4.5}
+              className="text-center colStyle2"
+              
+              duration={1.5}
             />
           </Row>
           {}
         </Col>
         <Col className="d-flex justify-content-center align-items-center"></Col>
       </Row>
-      <Row style={rowStyle}>
+      <Row className="rowStyle">
         <Col className="d-flex justify-content-center align-items-center">
           <Row className="mt-2">
-            <p className="text-center">Number of Trees Planted:</p>
+            <h4 className="text-center">Number of Trees Planted:</h4>
             <CountUp
               end={400}
-              className="text-center"
-              style={colStyle}
-              duration={4.5}
+              className="text-center colStyle"
+
+              duration={1.5}
             />{" "}
           </Row>
           {/* Adjust values as needed */}
         </Col>
         <Col className="d-flex justify-content-center align-items-center">
           <Row className="mt-2">
-            <p className="text-center">Tones of Plastic Recycled:</p>
+            <h4 className="text-center">Tones of Plastic Recycled:</h4>
             <CountUp
               end={300}
-              className="text-center"
-              style={colStyle}
-              duration={4.5}
+              className="text-center colStyle"
+            
+              duration={1.5}
             />{" "}
           </Row>
           {/* Adjust values as needed */}
         </Col>
         <Col className="d-flex justify-content-center align-items-center">
           <Row className="mt-2">
-            <p className="text-center">Thousands of creatures saved:</p>
+            <h4 className="text-center">Thousands of creatures saved:</h4>
             <CountUp
               end={200}
-              className="text-center"
-              style={colStyle}
-              duration={4.5}
+              className="text-center colStyle"
+          
+              duration={1.5}
             />
           </Row>
           {/* Adjust values as needed */}
         </Col>
       </Row>
+
+      
     </Container>
   );
 }
