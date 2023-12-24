cbec1662c1690f2717fd165cfc37e6a9543af849
AMANMUHAL <aman.muhal.ug21@nsut.ac.in>
2023-12-24 15:07:32 +0530

    button changed in goalsFinal

--- src/Components/GoalsFinal.jsx
+++ src/Components/GoalsFinal.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import "./styles/goalsFinal.css";
+import Button from 'react-bootstrap/Button';
 
 const PostsData = [
   {
@@ -11,9 +12,9 @@ const PostsData = [
       "https://images.pexels.com/photos/128421/pexels-photo-128421.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
-    category: "Travel",
+    category: "News",
     title: "Encouraging Recycling Practices:",
-    text: "Incentivizing recycling through awareness campaigns, rewards programs, or recognition for individuals or neighborhoods that actively engage in recycling.",
+    text: "Incentivizing recycling through awareness campaigns, rewards programs, or recognition for individuals or neighborhoods that actively engage in recycling.Motivating residents to actively participate in recycling efforts by providing accessible and convenient recycling facilities.",
     image:
       "https://images.pexels.com/photos/802221/pexels-photo-802221.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
@@ -21,21 +22,21 @@ const PostsData = [
   {
     category: "News",
     title: "Reducing Environmental Impact of Waste:",
-    text: "Encouraging the reuse of materials and advocating for the reduction of single-use items to minimize environmental degradation and resource depletion.",
+    text: "Reducing the amount of waste sent to landfills by promoting recycling and proper waste disposal techniques.Advocating for the reduction of single-use items to minimize degradation and depletion.",
     image:
       "https://images.pexels.com/photos/2547565/pexels-photo-2547565.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
-    category: "Travel",
+    category: "News",
     title: "Organizing Awareness Campaigns:",
-    text: "Using various communication channels, including social media, local newspapers, flyers, and community events, to reach a wide audience and promote the importance of recycling.",
+    text: "Conducting regular awareness campaigns, events, and workshops to engage the community, spread information about waste management best practices, and encourage participation in recycling initiatives.",
     image:
       "https://images.pexels.com/photos/3302183/pexels-photo-3302183.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
   {
-    category: "Travel",
+    category: "News",
     title: "Measuring and Monitoring Impact:",
-    text: "Utilizing feedback mechanisms to continuously improve and adapt the initiative based on community input and changing needs.",
+    text: "Implementing mechanisms to track and measure the effectiveness of the recycling initiative, such as quantifying the amount of waste diverted from landfills and evaluating environmental impact assessments",
     image:
       "https://images.pexels.com/photos/247763/pexels-photo-247763.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   },
@@ -72,11 +73,12 @@ const Title = () => {
   );
 };
 
-const Button = () => {
+const Buttoon = () => {
   return (
-    <button className="button button-primary">
-      <i className="fa fa-chevron-right"></i> Find out more
-    </button>
+    // <button className="button button-primary">
+    //   <i className="fa fa-chevron-right"></i> Find out more
+    // </button>
+    <Button variant="outline-secondary"><i className="fa fa-chevron-right"></i> Find out more</Button>
   );
 };
 
@@ -97,7 +99,7 @@ const CardBody = ({ title, text }) => {
       <p className="date">March 20 2023</p>
       <h2>{title}</h2>
       <p className="body-content">{text}</p>
-      <Button />
+      <Buttoon />
     </div>
   );
 };
