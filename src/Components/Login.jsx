0125eaf7d30fd738eed07e8e8f8163d5c7d3546d
Neeraj Sharma <neeraj716sharma@gmail.com>
2023-12-28 12:21:05 +0530

    Fix the login and signup page

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,54 +1,78 @@
-import {useState} from 'react';
-import {Link,useNavigate} from 'react-router-dom';
-import {auth} from "../firebase-config";
-import {signInWithEmailAndPassword} from 'firebase/auth';
-import Navbarr from './Navbar';
-import Footer from './Footer';
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../firebase-config";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import Navbarr from "./Navbar";
+import Footer from "./Footer";
 
 import "./styles/login.css";
 
+function Login({ setAuth }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  let navigate = useNavigate();
 
-function Login({setAuth}){
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    let navigate=useNavigate();
-
-    const handleSignUp = async (e) => {
-        e.preventDefault();
-        signInWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                // Signed in 
-                const user = userCredential.user;
-                setAuth(true);
-                navigate('/user');
-                // ...
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-            });
-      };
-    return <div className='login-page'>
-        <Navbarr/>
-        <div className="background">
-            <div className="shape"></div>
-            <div className="shape"></div>
-        </div>
-    <form style={{color:'black '}} className='formm'>
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        setAuth(true);
+        navigate("/user");
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  };
+  return (
+    <div className="login-page">
+      <Navbarr />
+      <div className="background mt-4">
+        <div className="shape mt-4"></div>
+        <div className="shape mt-4"></div>
+      </div>
+      <form style={{ color: "black " }} className="formm">
         <h3>Login</h3>
 
-        <label className='labell' >Email</label>
-        <input className='inputt' type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email or Phone" id="username"/>
+        <label className="labell">Email</label>
+        <input
+          className="inputt"
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email or Phone"
+          id="username"
+        />
 
-        <label className='labell' >Password</label>
-        <input className='inputt' type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" id="password"/>
+        <label className="labell">Password</label>
+        <input
+          className="inputt"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          id="password"
+        />
 
-        <button className='buttonn' onClick={handleSignUp}>Login</button><br/>
-        <p>New user? <Link to="/signup" style={{color:'black', textDecoration:'none'}}>Sign Up</Link></p>
-    </form>
-    <div className='footer'>
-    <Footer /></div>
+        <button className="buttonn" onClick={handleSignUp}>
+          Login
+        </button>
+        <br />
+        <p>
+          New user?{" "}
+          <Link to="/signup" style={{ color: "black", textDecoration: "none" }}>
+            Sign Up
+          </Link>
+        </p>
+      </form>
+      <div className="footer">
+        <Footer />
+      </div>
     </div>
+  );
 }
 
 export default Login;
--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,55 +1,72 @@
-import {useState} from 'react';
-import {useNavigate} from 'react-router-dom';
-import {auth} from "../firebase-config";
-import {createUserWithEmailAndPassword} from 'firebase/auth';
-import Navbarr from './Navbar';
-import Footer from './Footer';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../firebase-config";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import Navbarr from "./Navbar";
+import Footer from "./Footer";
 import "./styles/login.css";
 
+function SignUp({ setAuth }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  let navigate = useNavigate();
 
-function SignUp({setAuth}){
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    let navigate=useNavigate();
-
-    const handleSignUp = async (e) => {
-        e.preventDefault();
-        createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                // Signed up 
-                const user = userCredential.user;
-                setAuth(true);
-                console.log(user);
-                navigate('/user');
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                console.log(errorMessage);
-                // ..
-            });
-      };
-    return <div className='login-page'>
-        <Navbarr/>
-        <div className="background">
-            <div className="shape"></div>
-            <div className="shape"></div>
-        </div>
-    <form className=' formm' style={{color:'black'}}>
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        setAuth(true);
+        console.log(user);
+        navigate("/user");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage);
+        // ..
+      });
+  };
+  return (
+    <div className="login-page">
+      <Navbarr />
+      <div className="background mt-4">
+        <div className="shape mt-4"></div>
+        <div className="shape mt-4"></div>
+      </div>
+      <form className=" formm" style={{ color: "black" }}>
         <h3>Sign Up</h3>
 
-        <label className='labell'>Email</label>
-        <input className='inputt'  type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" id="username"/>
+        <label className="labell">Email</label>
+        <input
+          className="inputt"
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          id="username"
+        />
 
-        <label className='labell'>Password</label>
-        <input  className='inputt' type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" id="password"/>
+        <label className="labell">Password</label>
+        <input
+          className="inputt"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          id="password"
+        />
 
-        <button className='buttonn' onClick={handleSignUp}>Sign Up</button>
-    </form>
-    <div className='footer'>
-    <Footer /></div>
+        <button className="buttonn" onClick={handleSignUp}>
+          Sign Up
+        </button>
+      </form>
+      <div className="footer">
+        <Footer />
+      </div>
     </div>
+  );
 }
 
 export default SignUp;
-
