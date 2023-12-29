c2c344bc0e01a4bb262545db1b3b1f404bb8c884
Neeraj Sharma <neeraj716sharma@gmail.com>
2023-12-29 11:48:44 +0530

    fix icons of home page

--- src/Components/Heatmap.jsx
+++ src/Components/Heatmap.jsx
@@ -1,31 +1,110 @@
+// import React, { useEffect, useState } from "react";
+// import HeatMap from "@uiw/react-heat-map";
+// import { auth } from "../firebase-config";
+
+// const Heat = () => {
+//   const [signInDate, setSignInDate] = useState(null);
+
+//   useEffect(() => {
+//     const unsubscribe = auth.onAuthStateChanged((user) => {
+//       if (user) {
+//         // User is signed in
+//         const lastSignInTime = user.metadata.lastSignInTime;
+//         console.log(lastSignInTime);
+//         const lastSignInDate = new Date(lastSignInTime)
+//           .toISOString()
+//           .split("T")[0];
+//         setSignInDate(lastSignInDate);
+//       } else {
+//         // User is signed out
+//         setSignInDate(null);
+//       }
+//     });
+
+//     return () => unsubscribe(); // Cleanup on component unmount
+//   }, []);
+
+//   const value = [
+//     { date: signInDate, count: 10 }, // Mark the user's last sign-in date
+//   ];
+
+//   return (
+//     <div className="container mt-3">
+//       <h4 className="text-center fw-bold"> Heat Map</h4>
+//       <div className="row justify-content-center">
+//         <div className="col-md-12">
+//           <div className="w-100">
+//             <HeatMap
+//               className="w-100"
+//               width={500}
+//               space={3}
+//               legendCellSize={0}
+//               value={value}
+//               weekLabels={["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]}
+//               startDate={new Date("2023/12/1")}
+//               monthLabels={[
+//                 "Jan",
+//                 "Feb",
+//                 "Mar",
+//                 "Apr",
+//                 "May",
+//                 "Jun",
+//                 "Jul",
+//                 "Aug",
+//                 "Sep",
+//                 "Oct",
+//                 "Nov",
+//                 "Dec",
+//               ]}
+//             />
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Heat;
+
 import React, { useEffect, useState } from "react";
 import HeatMap from "@uiw/react-heat-map";
 import { auth } from "../firebase-config";
 
 const Heat = () => {
-  const [signInDate, setSignInDate] = useState(null);
+  const [signUpDate, setSignUpDate] = useState(null);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       if (user) {
         // User is signed in
-        const lastSignInTime = user.metadata.lastSignInTime;
-        console.log(lastSignInTime);
-        const lastSignInDate = new Date(lastSignInTime)
-          .toISOString()
-          .split("T")[0];
-        setSignInDate(lastSignInDate);
+        const signUpTime = user.metadata.creationTime;
+        console.log(signUpTime);
+        const signUpDate = new Date(signUpTime).toISOString().split("T")[0];
+        setSignUpDate(signUpDate);
       } else {
         // User is signed out
-        setSignInDate(null);
+        setSignUpDate(null);
       }
     });
 
     return () => unsubscribe(); // Cleanup on component unmount
   }, []);
 
+  const getLastNDays = (n) => {
+    const dates = [];
+    for (let i = 0; i < n; i++) {
+      const date = new Date();
+      date.setDate(date.getDate() - i);
+      dates.push(date.toISOString().split("T")[0]);
+    }
+    return dates;
+  };
+
+  const last10Days = getLastNDays(10);
+
   const value = [
-    { date: signInDate, count: 10 }, // Mark the user's last sign-in date
+    { date: signUpDate, count: 10 }, // Mark the user's sign-up date
+    ...last10Days.map((date) => ({ date, count: 10 })), // Mark the last 10 days
   ];
 
   return (
--- src/Components/Points.jsx
+++ src/Components/Points.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React from "react";
 import "./styles/points.css";
 
 function Points() {
   return (
-    <div className='pointsComponent'>
-    
+    <div className="pointsComponent">
       <div className="card-wrapper one">
         <div className="card-header">
-          <i className="fas fa-chess-rook"></i>
+          <i class="fa-solid fa-user-plus"></i>
           <h2 className="card-header-text1">Sign </h2>
           <h1 className="card-header-text2">Up</h1>
           <p className="card-header-text3">
-          Sign Up to our website and get 100 points as a welcome bonus. Dont worry its free and will always will be. Just go to the sign up page and fill in the details.
+            Sign Up to our website and get 100 points as a welcome bonus. Dont
+            worry its free and will always will be. Just go to the sign up page
+            and fill in the details.
           </p>
         </div>
         <div className="card-footer">
@@ -30,7 +31,9 @@ function Points() {
           <h2 className="card-header-text1">Earn </h2>
           <h1 className="card-header-text2">Points</h1>
           <p className="card-header-text3">
-          Earn points by participating in activities such as planting or adopting trees, joining environmental education programs, taking quizzes, and signing up for newsletters.
+            Earn points by participating in activities such as planting or
+            adopting trees, joining environmental education programs, taking
+            quizzes, and signing up for newsletters.
           </p>
         </div>
         <div className="card-footer">
@@ -45,11 +48,12 @@ function Points() {
       </div>
       <div className="card-wrapper three">
         <div className="card-header">
-          <i className="fas fa-gamepad"></i>
+          <i class="fa-solid fa-gift"></i>
           <h2 className="card-header-text1">Redeem </h2>
           <h1 className="card-header-text2">Points</h1>
           <p className="card-header-text3">
-          Redeem your points for rewards such as discounts, coupons, and freebies from our partner recycled product stores.
+            Redeem your points for rewards such as discounts, coupons, and
+            freebies from our partner recycled product stores.
           </p>
         </div>
         <div className="card-footer">
