80ae81d6cf005925719016404cbfa3a3d7ac4f10
Neeraj Sharma <neeraj716sharma@gmail.com>
2023-12-29 23:36:03 +0530

    Video Added if pasand na aaye to revert kar lena movingBg.jsx me

--- src/Components/movingBg.jsx
+++ src/Components/movingBg.jsx
@@ -3,80 +3,81 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import CountUp from "react-countup";
-import Bgvideo from "./Video/video.mp4"; //video is from Ingrid North
+import Bgvideo from "./Video/Video_2.mp4"; //video is from Ingrid North
 import "./styles/style.css";
 // import GoalsFinal from "./goalsFinal";
 // import News from "./News";
 
 function MovingBg() {
-  return (<>
-    <Container fluid className="mt-0 p-0 containerStyle">
-      <video autoPlay loop muted className="bg-vid videoStyle">
-        <source src={Bgvideo} type="video/mp4" />
-      </video>
+  return (
+    <>
+      <Container fluid className="mt-0 p-0 containerStyle">
+        <video autoPlay loop muted className="bg-vid videoStyle">
+          <source src={Bgvideo} type="video/mp4" />
+        </video>
 
-      <Row className="rowStyle">
-        <Col className="d-flex justify-content-center align-items-center">
-          {}
-        </Col>
-        <Col className="d-flex justify-content-center align-items-center">
-          {/* Adjust values as needed */}
-        </Col>
-        <Col className="d-flex justify-content-center align-items-center">
-          {/* Adjust values as needed */}
-        </Col>
-      </Row>
-      <Row className="rowStyle">
-        <Col className="d-flex justify-content-center align-items-center"></Col>
+        <Row className="rowStyle">
+          <Col className="d-flex justify-content-center align-items-center">
+            {}
+          </Col>
+          <Col className="d-flex justify-content-center align-items-center">
+            {/* Adjust values as needed */}
+          </Col>
+          <Col className="d-flex justify-content-center align-items-center">
+            {/* Adjust values as needed */}
+          </Col>
+        </Row>
+        <Row className="rowStyle">
+          <Col className="d-flex justify-content-center align-items-center"></Col>
 
-        <Col className="d-flex justify-content-center align-items-center">
-          <Row className="mt-2">
-            <h4 className="text-center">Number of Trees Planted:</h4>
-            <CountUp
-              end={500}
-              className="text-center colStyle2"
-              duration={1.5}
-            />
-          </Row>
-          {}
-        </Col>
-        <Col className="d-flex justify-content-center align-items-center"></Col>
-      </Row>
-      <Row className="rowStyle">
-        <Col className="d-flex justify-content-center align-items-center">
-          <Row className="">
-            <h5 className="text-center">Number of Trees Planted:</h5>
-            <CountUp
-              end={400}
-              className="text-center colStyle"
-              duration={1.5}
-            />{" "}
-          </Row>
-          {/* Adjust values as needed */}
-        </Col>
-        <Col className="d-flex justify-content-center align-items-center">
-          <Row className="mt-2">
-            <h5 className="text-center">Tones of Plastic Recycled:</h5>
-            <CountUp
-              end={300}
-              className="text-center colStyle"
-              duration={1.5}
-            />{" "}
-          </Row>
-          {/* Adjust values as needed */}
-        </Col>
-        <Col className="d-flex justify-content-center align-items-center">
-          <Row className="mt-2">
-            <h5 className="text-center">Thousands of creatures saved:</h5>
-            <CountUp
-              end={200}
-              className="text-center colStyle"
-              duration={1.5}
-            />
-          </Row>
-        </Col>
-      </Row>
-    </Container>
+          <Col className="d-flex justify-content-center align-items-center">
+            <Row className="mt-2">
+              <h4 className="text-center">Number of Trees Planted:</h4>
+              <CountUp
+                end={500}
+                className="text-center colStyle2"
+                duration={1.5}
+              />
+            </Row>
+            {}
+          </Col>
+          <Col className="d-flex justify-content-center align-items-center"></Col>
+        </Row>
+        <Row className="rowStyle">
+          <Col className="d-flex justify-content-center align-items-center">
+            <Row className="">
+              <h5 className="text-center">Number of Trees Planted:</h5>
+              <CountUp
+                end={400}
+                className="text-center colStyle"
+                duration={1.5}
+              />{" "}
+            </Row>
+            {/* Adjust values as needed */}
+          </Col>
+          <Col className="d-flex justify-content-center align-items-center">
+            <Row className="mt-2">
+              <h5 className="text-center">Tones of Plastic Recycled:</h5>
+              <CountUp
+                end={300}
+                className="text-center colStyle"
+                duration={1.5}
+              />{" "}
+            </Row>
+            {/* Adjust values as needed */}
+          </Col>
+          <Col className="d-flex justify-content-center align-items-center">
+            <Row className="mt-2">
+              <h5 className="text-center">Thousands of creatures saved:</h5>
+              <CountUp
+                end={200}
+                className="text-center colStyle"
+                duration={1.5}
+              />
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
